2554aff828e405f7ddd38809005dc448a78b6894
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-04-14 20:44:15 -0600

    add: navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+export const NavBar = () => {
+	return (
+		<nav className='bg-gray-600 text-white h-10 flex items-center justify-end p-6'>
+			<a href='/' className='bg-gray-300 p-2 rounded-lg'>
+				Login
+			</a>
+		</nav>
+	)
+}
